93e2bd0b1475a8278b6263dbabbd7d573e90488d
stishy <cameronjstish@gmail.com>
2024-03-13 11:47:04 -0400

    Update netexec-fix.ps1

--- netexec-fix.ps1
+++ netexec-fix.ps1
@@ -1,16 +1,16 @@
 ﻿$RegistryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
 $RegistryName = "LocalAccountTokenFilterPolicy"
-$Value = 1
+$DesiredValue = 1
 
 # Check if the registry key exists
 if (-not (Test-Path -Path "$RegistryPath\$RegistryName")) {
     # Create the registry key
-    New-ItemProperty -Path $RegistryPath -Name $RegistryName -Value $Value -PropertyType DWORD -Force
+    New-ItemProperty -Path $RegistryPath -Name $RegistryName -Value $DesiredValue -PropertyType DWORD -Force
 } else {
     # If the registry key exists, check its value
     $CurrentValue = (Get-ItemProperty -Path "$RegistryPath" -Name "$RegistryName").$RegistryName
-    if ($CurrentValue -ne $Value) {
+    if ($CurrentValue -ne $DesiredValue) {
         # Set the value to the desired value if it's different
-        Set-ItemProperty -Path $RegistryPath -Name $RegistryName -Value $Value
+        Set-ItemProperty -Path $RegistryPath -Name $RegistryName -Value $DesiredValue
     }
 }
